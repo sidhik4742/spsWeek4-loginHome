5faedaa646e5f12c1fc1d090b67cfebd9c28dc8a
sidhik4742 <sidhik64742@gmail.com>
2020-10-05 15:23:24 +0530

    #initial commit completed validation in the login page

---
+++ public/javascripts/login.js
@@ -0,0 +1,35 @@
+const validaton = () => {
+  //   console.log("Enter");
+  if (userName.value === "" || userName.value.length <= 4) {
+    console.error("Please enter a valid user name");
+    userName.style.borderColor = "red";
+    userNameErr.style.display = "block";
+    return false;
+  } else if (password.value === "" || password.value.length <= 4) {
+    userName.style.borderColor = "grey";
+    userNameErr.style.display = "none";
+    console.error("Please enter a valid password");
+    password.style.borderColor = "red";
+    passwordErr.style.display = "block";
+    return false;
+  } else {
+    password.style.borderColor = "grey";
+    passwordErr.style.display = "none";
+    return true;
+  }
+};
+
+let userName = document.getElementById("username");
+let password = document.getElementById("password");
+let userNameErr = document.getElementById("userNameErr");
+let passwordErr = document.getElementById("passwordErr");
+
+document.getElementById("submitBtn").addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (validaton()) {
+    alert(" form submited");
+    userName.value = "";
+    password.value = "";
+  }
+});
