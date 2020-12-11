e4002906973438a3f4c7081a1f2cf446d6479f95
Sidhik AR <sidhik64742@gmail.com>
2020-12-11 15:05:40 +0530

    change mongo db url

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 // const objectId = require("mongodb").ObjectID;
-const config = require("../config/config");
+const config = require('../config/config');
 
 let state = {
   db: null,
@@ -9,19 +9,21 @@ let state = {
 module.exports = {
   dbConnect: (callback) => {
     try {
-      const url = "mongodb://localhost:27017";
-      const dbName = "customerDetails";
-      MongoClient.connect(url, { useUnifiedTopology: true }, (error, db) => {
+      const url =
+        'mongodb+srv://sidhik:letmein64742@cluster0.wgcac.mongodb.net/eLibraryDB?retryWrites=true&w=majority' ||
+        'mongodb://localhost:27017';
+      const dbName = 'customerDetails';
+      MongoClient.connect(url, {useUnifiedTopology: true}, (error, db) => {
         if (error) {
           return callback(error);
         }
-        console.log("Connected successfully to server");
+        console.log('Connected successfully to server');
         //   console.log(db);
         state.db = db.db(dbName);
         callback();
       });
     } catch (error) {
-      console.log("Database connection error" + error);
+      console.log('Database connection error' + error);
     }
   },
 };
